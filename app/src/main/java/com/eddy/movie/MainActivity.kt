4bc0f12708e878cfea1f916fa00ddfe3039af9d6
yujin <jini1514@soongsil.ac.kr>
2023-10-19 16:53:00 +0900

    add: viewBinding

--- app/src/main/java/com/eddy/movie/MainActivity.kt
+++ app/src/main/java/com/eddy/movie/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.eddy.movie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eddy.movie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
