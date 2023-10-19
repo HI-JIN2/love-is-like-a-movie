ee1af94f8cb029c8dcd1766cbce12cbcfe19db7a
yujin <jini1514@soongsil.ac.kr>
2023-10-19 20:00:56 +0900

    feat: datasource

---
+++ app/src/main/java/com/eddy/movie/data/repository/MovieDetailsDataSource.kt
@@ -0,0 +1,46 @@
+package com.eddy.movie.data.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.eddy.movie.data.api.MovieService
+import com.eddy.movie.data.vo.MovieDetailResponseDto
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MovieDetailsDataSource (private val apiService: MovieService, private val compositeDisposable: CompositeDisposable){
+
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState: LiveData<NetworkState>
+        get() = _networkState
+
+    private val _downloadedMovieDetailsResponse = MutableLiveData<MovieDetailResponseDto>()
+    val downloadedMovieDetailsResponse: LiveData<MovieDetailResponseDto>
+        get() = _downloadedMovieDetailsResponse
+
+    fun fetchMovieDetails(movieId: Int){
+        _networkState.postValue(NetworkState.LOADING)
+
+        try {
+            compositeDisposable.add(
+                apiService.getMovieDetails(movieId)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe (
+                        {
+                            _downloadedMovieDetailsResponse.postValue(it)
+                            _networkState.postValue(NetworkState.LOADED)
+                        },
+                        {
+                            _networkState.postValue(NetworkState.ERROR)
+                            it.message?.let { it1 -> Log.e("MovieDetailsDataSource", it1) }
+
+                        }
+                    )
+            )
+
+        }catch(e: Exception){
+            e.message?.let { Log.e("MovieDetailsDataSource", it) }
+        }
+    }
+}
